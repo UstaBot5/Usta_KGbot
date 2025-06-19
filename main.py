52f6d1f131020d1b08500be83b61d696dd095986
UstaBot5 <jerulan82@mail.ru>
2025-06-19 17:15:46 +0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+from telegram.ext import Updater, CommandHandler
+
+def start(update, context):
+    update.message.reply_text('Добро пожаловать в Usta KG!')
+
+updater = Updater(7901632898:AAFF5UShYE8-cEC71ABnJX9gC_zpClwBtZQ, use_context=True)
+dp = updater.dispatcher
+dp.add_handler(CommandHandler("start", start))
+updater.start_polling()
+updater.idle()
